2eaa6b3bc2df951fb8504235a3266717b339bfe3
KleprlikJan <kleprlikjan@gmail.com>
2021-10-17 01:34:11 +0200

    Update js sreality crawler

--- main.js
+++ main.js
@@ -10,11 +10,26 @@ const { handleStart, handleList, handleDetail } = require('./src/routes');
 const { utils: { log } } = Apify;
 
 Apify.main(async() => {
+    log.setLevel(log.LEVELS.DEBUG);
+    const myRequest = new Apify.Request({
+        url: 'https://www.sreality.cz/api/cs/v2/estates?building_condition=11&category_main_cb=2&category_type_cb=1&czk_price_summary_order2=5000000&locality_region_id=10&per_page=100&room_count_cb=1&tms=1634393131332',
+        headers: { Accept: 'application/json' },
+    });
+
+
+
     const { startUrls } = await Apify.getInput();
+
+
+
 
     const requestList = await Apify.openRequestList('start-urls', startUrls);
     const requestQueue = await Apify.openRequestQueue();
     const proxyConfiguration = await Apify.createProxyConfiguration();
+
+    const additionalMimeTypes = ["application/hal+json"]
+
+    await requestQueue.addRequest(myRequest);
 
     const crawler = new Apify.CheerioCrawler({
         requestList,
@@ -23,6 +38,20 @@ Apify.main(async() => {
         // Be nice to the websites.
         // Remove to unleash full power.
         maxConcurrency: 50,
+        additionalMimeTypes: additionalMimeTypes,
+        postNavigationHooks: [
+            async(crawlingContext) => {
+                // var request = crawlingContext.response;
+                // log.debug(request)
+                // for (const [key, value] of Object.entries(request)) {
+                //     log.debug(`${key}: ${value}`);
+                // }
+                // log.debug("-----------_readableState------------")
+                // for (const [key, value] of Object.entries(request['_readableState'])) {
+                //     log.debug(`${key}: ${value}`);
+                // }
+            },
+        ],
         handlePageFunction: async(context) => {
             const { url, userData: { label } } = context.request;
             log.info('Page opened.', { label, url });
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,37 @@ const Apify = require('apify');
 
 const { utils: { log } } = Apify;
 
-exports.handleStart = async ({ request, $ }) => {
+exports.handleStart = async({ request, $ }) => {
     // Handle Start URLs
+    log.info("[START]: Start")
+        // log.debug(request)
+        // for (const [key, value] of Object.entries(request)) {
+        //     log.debug(`${key}: ${value}`);
+        // }
+        // log.debug("-----------HEADERS------------")
+        // for (const [key, value] of Object.entries(request['headers'])) {
+        //     log.debug(`${key}: ${value}`);
+        // }
+        // log.debug("------------userData-------------")
+        // for (const [key, value] of Object.entries(request['userData'])) {
+        //     log.debug(`${key}: ${value}`);
+        // }
+    log.debug($)
+    log.info("[START]: End")
 };
 
-exports.handleList = async ({ request, $ }) => {
+exports.handleList = async({ request, $ }) => {
     // Handle pagination
+
+    log.info("[LIST]: Start")
+
+    log.info("[LIST]: End")
 };
 
-exports.handleDetail = async ({ request, $ }) => {
-    // Handle details
+exports.handleDetail = async({ request, $ }) => {
+    // Handle 
+
+    log.info("[DETAIL]: Start")
+
+    log.info("[DETAIL]: End")
 };
