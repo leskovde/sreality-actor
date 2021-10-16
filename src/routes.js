c59130481934ce2f34da4e7a034b42dbf6b93dd0
leskovde <leskovar.denis@seznam.cz>
2021-10-16 20:48:35 +0200

    Detail added

--- src/routes.js
+++ src/routes.js
@@ -12,4 +12,47 @@ exports.handleList = async ({ request, page }) => {
 
 exports.handleDetail = async ({ request, page }) => {
     // Handle details
+    log.info("[DETAIL]: Getting detail info.");
+
+    let advertContainer = await page.$$eval(".advert-detail-fixed-top__content-info", $list => {
+        const item = Array.from($list)[0];
+        
+        //return {"xd" : item.innerHTML};
+
+        const headerText = item.querySelector("h4").textContent;
+        const sellingType = headerText.split("-")[0].trim();
+
+        const buildingType = headerText.split("-")[1].split(",")[0];
+        const location = item.querySelector("p").textContent.trim();
+        
+        const highlight = item.querySelector("strong");
+        const price = highlight.querySelector("span").textContent.trim();
+
+        flag = true;
+
+        return [
+            {"sellingType" : sellingType}, 
+            {"buildingType" : buildingType},
+            {"location" : location},
+            {"price" : price}
+        ]
+    });
+
+    let propertyElList = await page.$$eval(".detail-information__data-item", $list => {
+
+        const items = [];
+
+        $list.forEach($item => {
+            const key = $item.querySelector("span:nth-child(1)").textContent.trim();
+            const value = $item.querySelector("span:nth-child(2)").textContent.trim();
+
+            items.push({ 'key': key, 'value' : value });
+        });
+        
+        return items;
+    });
+
+    await Apify.pushData({"header" : advertContainer, "properties" : propertyElList});
+
+    log.info("[DETAIL]: Detail info done.");
 };
